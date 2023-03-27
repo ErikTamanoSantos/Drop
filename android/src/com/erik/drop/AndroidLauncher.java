57243e283dc7adab72461047f17536392c5ca850
ErikTamano <72156568+ErikTamanoSantos@users.noreply.github.com>
2023-03-27 16:28:25 +0200

    Tutorial pt1 finished

---
+++ android/src/com/erik/drop/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.erik.drop;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.erik.drop.DropGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new DropGame(), config);
+	}
+}
